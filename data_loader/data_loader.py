5121b1cb87e308bd7828369505bf57f2830825c5
parthpankajtiwary <parthpankajtiwary@gmail.com>
2019-10-18 16:28:37 +0200

    readme update | template initialization

--- data_loader/data_loader.py
+++ data_loader/data_loader.py
@@ -1,13 +1,11 @@
 from base.base_data_loader import BaseDataLoader
-from keras.datasets import mnist
 
-
-class SimpleMnistDataLoader(BaseDataLoader):
+class DataLoader(BaseDataLoader):
     def __init__(self, config):
-        super(SimpleMnistDataLoader, self).__init__(config)
+        super(ConvMnistDataLoader, self).__init__(config)
         (self.X_train, self.y_train), (self.X_test, self.y_test) = mnist.load_data()
-        self.X_train = self.X_train.reshape((-1, 28 * 28))
-        self.X_test = self.X_test.reshape((-1, 28 * 28))
+        self.X_train = self.X_train.reshape((-1, 28, 28, 1))
+        self.X_test = self.X_test.reshape((-1, 28, 28, 1))
 
     def get_train_data(self):
         return self.X_train, self.y_train
